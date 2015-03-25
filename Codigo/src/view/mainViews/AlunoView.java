8d05dec3df6512441d476de85a53234578a197bd
fmarques55 <felipesampaio93@hotmail.com>
2015-03-25 16:42:44 -0300

    Aplicando padrão folha de estilo classe ClienteView

--- Codigo/src/view/mainViews/AlunoView.java
+++ Codigo/src/view/mainViews/AlunoView.java
@@ -62,23 +62,23 @@ public class AlunoView extends ClienteView {
     @Override public void excluirAction() {
         try {
             int index = this.tabelaCliente.getSelectedRow();
-            if (index < 0) {
+            if ( index < 0 ) {
                 JOptionPane.showMessageDialog(this, "Selecione uma linha!", "Erro", JOptionPane.ERROR_MESSAGE, null);
                 return;
             }
 
-            int confirm = JOptionPane.showConfirmDialog(this, "Deseja mesmo excluir Aluno: "
+            int confirm = JOptionPane.showConfirmDialog( this, "Deseja mesmo excluir Aluno: "
                     + ManterAluno.getInstance().getAluno_vet().get(index).getNome() + "?", "Excluir", JOptionPane.YES_NO_OPTION);
-            if (confirm == JOptionPane.YES_OPTION) {
-                ManterAluno.getInstance().excluir(ManterAluno.getInstance().getAluno_vet().get(index));
-                JOptionPane.showMessageDialog(this, "Aluno excluido com sucesso", "Sucesso", JOptionPane.INFORMATION_MESSAGE, null);
+            if (confirm == JOptionPane.YES_OPTION ) {
+                ManterAluno.getInstance().excluir( ManterAluno.getInstance().getAluno_vet().get(index) );
+                JOptionPane.showMessageDialog( this, "Aluno excluido com sucesso", "Sucesso", JOptionPane.INFORMATION_MESSAGE, null );
             }
-            this.tabelaCliente.setModel(fillTable());
+            this.tabelaCliente.setModel( fillTable() );
 
-        } catch (ClienteException ex) {
-            JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
+        } catch ( ClienteException ex ) {
+            JOptionPane.showMessageDialog( this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null );
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
+            JOptionPane.showMessageDialog( this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null );
         }
     }
 
