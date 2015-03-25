846bbe48fc14538a410c8bb4df226106dd595d84
fmarques55 <felipesampaio93@hotmail.com>
2015-03-25 17:02:20 -0300

    Aplicando stylesheet na classe SalaView

--- Codigo/src/view/mainViews/SalaView.java
+++ Codigo/src/view/mainViews/SalaView.java
@@ -25,23 +25,23 @@ import exception.PatrimonioException;
  */
 public class SalaView extends PatrimonioView {
 
-    public SalaView(java.awt.Frame parent, boolean modal) {
-        super(parent, modal);
-        pesquisarLbl.setText("Digite a sala desejada: ");
-        this.setName("SalaView");
+    public SalaView( java.awt.Frame parent, boolean modal ) {
+        super( parent, modal );
+        pesquisarLbl.setText( "Digite a sala desejada: " );
+        this.setName( "SalaView" );
     }
 
-    protected Vector<String> fillDataVector(Sala sala) {
+    protected Vector<String> fillDataVector( Sala sala ) {
 
-        if (sala == null) {
+        if ( sala == null ) {
             return null;
         }
 
         Vector<String> nomesTabela = new Vector<String>();
 
-        nomesTabela.add(sala.getCodigo());
-        nomesTabela.add(sala.getDescricao());
-        nomesTabela.add(sala.getCapacidade());
+        nomesTabela.add( sala.getCodigo() );
+        nomesTabela.add( sala.getDescricao() );
+        nomesTabela.add( sala.getCapacidade() );
 
         return nomesTabela;
 
@@ -53,69 +53,69 @@ public class SalaView extends PatrimonioView {
 
             Iterator<Sala> i = ManterSala.getInstance().getSalas_vet().iterator();
 
-            table.addColumn("Codigo");
-            table.addColumn("Nome");
-            table.addColumn("Capacidade");
-            while (i.hasNext()) {
+            table.addColumn( "Codigo" );
+            table.addColumn( "Nome" );
+            table.addColumn( "Capacidade" );
+            while ( i.hasNext() ) {
                 Sala sala = i.next();
-                table.addRow(fillDataVector(sala));
+                table.addRow( fillDataVector(sala) );
             }
 
             return table;
 
-        } catch (PatrimonioException ex) {
-            JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
+        } catch ( PatrimonioException ex ) {
+            JOptionPane.showMessageDialog( this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null );
+        } catch ( SQLException ex ) {
+            JOptionPane.showMessageDialog( this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null );
         }
 
         return null;
     }
 
     @Override protected void cadastrarAction() {
-        CadastroPatrimonio cadastro = new CadastroSala(new javax.swing.JFrame(), true);
-        cadastro.setResizable(false);
-        cadastro.setVisible(true);
-        this.tabelaPatrimonio.setModel(fillTable());
+        CadastroPatrimonio cadastro = new CadastroSala( new javax.swing.JFrame(), true );
+        cadastro.setResizable( false );
+        cadastro.setVisible( true );
+        this.tabelaPatrimonio.setModel( fillTable() );
     }
 
-    @Override protected void alterarAction(int index) {
+    @Override protected void alterarAction( int index ) {
 
-        AlterarSala alteracao = new AlterarSala(new javax.swing.JFrame(), true, index);
-        alteracao.setResizable(false);
-        alteracao.setVisible(true);
-        this.tabelaPatrimonio.setModel(fillTable());
+        AlterarSala alteracao = new AlterarSala( new javax.swing.JFrame(), true, index );
+        alteracao.setResizable( false );
+        alteracao.setVisible( true );
+        this.tabelaPatrimonio.setModel( fillTable() );
     }
 
-    @Override protected void excluirAction(int index) {
+    @Override protected void excluirAction( int index ) {
         try {
             int confirm = JOptionPane
-                    .showConfirmDialog(this, "Deseja mesmo excluir Sala: "
+                    .showConfirmDialog( this, "Deseja mesmo excluir Sala: "
                             + ManterSala.getInstance().getSalas_vet().get(index).getDescricao() + "?", "Excluir",
-                            JOptionPane.YES_NO_OPTION);
+                            JOptionPane.YES_NO_OPTION );
 
-            if (confirm == JOptionPane.YES_OPTION) {
-                ManterSala.getInstance().excluir(ManterSala.getInstance().getSalas_vet().get(index));
-                JOptionPane.showMessageDialog(this, "Sala excluida com sucesso", "Sucesso", JOptionPane.INFORMATION_MESSAGE, null);
+            if ( confirm == JOptionPane.YES_OPTION ) {
+                ManterSala.getInstance().excluir( ManterSala.getInstance().getSalas_vet().get( index) );
+                JOptionPane.showMessageDialog(this, "Sala excluida com sucesso", "Sucesso", JOptionPane.INFORMATION_MESSAGE, null );
             }
-            this.tabelaPatrimonio.setModel(fillTable());
+            this.tabelaPatrimonio.setModel( fillTable() );
 
-        } catch (PatrimonioException ex) {
-            JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
+        } catch ( PatrimonioException ex ) {
+            JOptionPane.showMessageDialog( this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null );
+        } catch ( SQLException ex ) {
+            JOptionPane.showMessageDialog( this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null );
         }
     }
 
-    @Override protected void visualizarAction(int index) {
+    @Override protected void visualizarAction( int index ) {
         try {
-            DiaReservaSala reserva = new DiaReservaSala(new javax.swing.JFrame(), true, index);
-            reserva.setResizable(false);
-            reserva.setVisible(true);
-        } catch (PatrimonioException ex) {
-            JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
+            DiaReservaSala reserva = new DiaReservaSala( new javax.swing.JFrame(), true, index );
+            reserva.setResizable( false );
+            reserva.setVisible( true );
+        } catch ( PatrimonioException ex ) {
+            JOptionPane.showMessageDialog( this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null );
+        } catch ( SQLException ex ) {
+            JOptionPane.showMessageDialog( this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null );
         }
     }
 }
