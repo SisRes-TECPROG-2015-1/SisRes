ee5d30debb1cf05f948222537e5b0d5ace906317
fmarques55 <felipesampaio93@hotmail.com>
2015-03-26 11:15:19 -0300

    Apllying pattern in class CadastroAluno

--- Codigo/src/view/cadastros/CadastroAluno.java
+++ Codigo/src/view/cadastros/CadastroAluno.java
@@ -17,28 +17,28 @@ import exception.ClienteException;
  */
 public class CadastroAluno extends CadastroCliente {
 
-    public CadastroAluno(java.awt.Frame parent, boolean modal) {
-        super(parent, modal);
-        this.setName("CadastroAluno");
+    public CadastroAluno( java.awt.Frame parent, boolean modal ) {
+        super( parent, modal );
+        this.setName( "CadastroAluno" );
     }
 
     @Override public void cadastroAction() {
         try {
-            if (cadastroBtn.getText().equals("Cadastrar")) {
-                ManterAluno.getInstance().inserir(nomeTxtField.getText(), cpfTxtField.getText(), matriculaTxtField.getText(),
-                        telefoneTxtField.getText(), emailTxtField.getText());
+            if ( cadastroBtn.getText().equals( "Cadastrar" ) ) {
+                ManterAluno.getInstance().inserir( nomeTxtField.getText(), cpfTxtField.getText(), matriculaTxtField.getText(),
+                        telefoneTxtField.getText(), emailTxtField.getText() );
 
-                JOptionPane.showMessageDialog(this, "Aluno Cadastrado com sucesso", "Sucesso", JOptionPane.INFORMATION_MESSAGE,
-                        null);
+                JOptionPane.showMessageDialog( this, "Aluno Cadastrado com sucesso", "Sucesso", JOptionPane.INFORMATION_MESSAGE,
+                        null );
 
-                this.setVisible(false);
+                this.setVisible( false );
             }
-        } catch (ClienteException ex) {
-            JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
+        } catch ( ClienteException ex ) {
+            JOptionPane.showMessageDialog( this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null );
+        } catch ( SQLException ex ) {
+            JOptionPane.showMessageDialog( this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null );
         } catch (NullPointerException ex) {
-            JOptionPane.showMessageDialog(this, ex.getLocalizedMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
+            JOptionPane.showMessageDialog( this, ex.getLocalizedMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null );
         }
     }
 }
