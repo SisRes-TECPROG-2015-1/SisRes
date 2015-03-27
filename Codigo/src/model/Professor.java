d2467785d7e3dc8a8f9f72d83ae759f6da607da9
Macário Soares <macario.junior@gmail.com>
2015-03-27 10:53:57 -0300

    Updating Professor model to programming technique #3

--- Codigo/src/model/Professor.java
+++ Codigo/src/model/Professor.java
@@ -22,20 +22,20 @@ public class Professor extends Cliente {
 	 * @param email
 	 * @throws ClienteException
 	 */
-	public Professor(String nome, String cpf, String matricula,
-			String telefone, String email) throws ClienteException {
-		super(nome, cpf, matricula, telefone, email);
+	public Professor( String nome, String cpf, String matricula,
+			String telefone, String email ) throws ClienteException {
+		super( nome, cpf, matricula, telefone, email );
 	}
 
 	/**
 	 * Setter method for attribute 'Matricula'
 	 * @throws ClienteException
 	 */
-	public void setMatricula(String matricula) throws ClienteException {
+	public void setMatricula( String matricula ) throws ClienteException {
 		if ( matricula == null ) {
-			throw new ClienteException(MATRICULA_NULO);
-		} else if ( "".equals(matricula.trim()) ) {
-			throw new ClienteException(MATRICULA_BRANCO);
+			throw new ClienteException( MATRICULA_NULO );
+		} else if ( "".equals( matricula.trim() ) ) {
+			throw new ClienteException( MATRICULA_BRANCO );
 		} else { 
 			// Nothing to do 
 		}
