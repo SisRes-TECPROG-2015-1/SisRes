08b295ac900e9d8491029b35ccabf283093e4c4d
Macário Soares <macario.junior@gmail.com>
2015-03-29 00:11:45 -0300

    Updating model 'Reserva', applying stylesheet and method's comments

--- Codigo/src/model/Reserva.java
+++ Codigo/src/model/Reserva.java
@@ -9,92 +9,138 @@ public class Reserva {
 
 	private String hora;
 	private String data;
-	
-	//Mesagens, Alertas e Padroes
-		private final String HORA_NULA = "A hora esta nula.";
-		private final String HORA_INVALIDA = "A hora eh invalida.";
-		private final String HORA_BRANCA = "A hora esta em branco.";
-		private final String HORA_PATTERN = "^[012]?[\\d]:[0-5][\\d]$";
-		private final String DATA_NULA = "A data esta nula.";
-		private final String DATA_INVALIDA = "A data eh invalida.";
-		private final String DATA_BRANCA = "A data esta em branco.";
-		private final String DATA_PATTERN = "^[0123]?[\\d]([./-])[01]?[\\d]\\1[\\d]{2,4}$";
-	
-	public Reserva(String data, String hora) throws ReservaException {
-		this.setData(data);
-		this.setHora(hora);
-	}
 
+	// Defining constants for error messages and alerts.
+	private final String HORA_NULA = "A hora esta nula.";
+	private final String HORA_INVALIDA = "A hora eh invalida.";
+	private final String HORA_BRANCA = "A hora esta em branco.";
+	private final String HORA_PATTERN = "^[012]?[\\d]:[0-5][\\d]$";
+	private final String DATA_NULA = "A data esta nula.";
+	private final String DATA_INVALIDA = "A data eh invalida.";
+	private final String DATA_BRANCA = "A data esta em branco.";
+	private final String DATA_PATTERN = "^[0123]?[\\d]([./-])[01]?[\\d]\\1[\\d]{2,4}$";
+	
+	/**
+	 * Constructor for class Reserva
+	 * @param data
+	 * @param hora
+	 * @throws ReservaException
+	 */
+	public Reserva( String data, String hora ) throws ReservaException {
+		this.setData( data );
+		this.setHora( hora );
+	}
+	
+	/**
+	 * Function to get the time value form 'hora' attribute
+	 * @return String hora
+	 */
 	public String getHora() {
 		return this.hora;
 	}
 
+	/**
+	 * Function to get the date value form 'data' attribute
+	 * @return
+	 */
 	public String getData() {
 		return this.data;
 	}
 
-	public void setHora(String hora) throws ReservaException {
-		if(hora == null)
-			throw new ReservaException(HORA_NULA);
-		
+	/**
+	 * Setter function for 'hora' attribute
+	 * @param hora
+	 * @throws ReservaException
+	 */
+	public void setHora( String hora ) throws ReservaException {
+		if ( hora == null )
+			throw new ReservaException( HORA_NULA );
+
 		hora = hora.trim();
-		if(hora.equals(""))
-			throw new ReservaException(HORA_BRANCA);
-		else if(hora.matches(HORA_PATTERN)){
-			if(hora.length() == 4)
+		if ( hora.equals( "" ) )
+			throw new ReservaException( HORA_BRANCA );
+		else if ( hora.matches( HORA_PATTERN ) ) {
+			if ( hora.length() == 4 ){
 				this.hora = "0" + hora;
-			else
+			} else {
 				this.hora = hora;
+			}
+		} else { 
+			throw new ReservaException( HORA_INVALIDA );
 		}
-		else
-			throw new ReservaException(HORA_INVALIDA);
 	}
 
-	public void setData(String data) throws ReservaException {
-		if(data == null)
+	/**
+	 * Setter function for 'data'attribute
+	 * @param data
+	 * @throws ReservaException
+	 */
+	public void setData( String data ) throws ReservaException {
+		if ( data == null ) {
 			throw new ReservaException(DATA_NULA);
-		
-		data = data.trim();
-		if(data.equals(""))
-			throw new ReservaException(DATA_BRANCA);
-		else if(data.matches(DATA_PATTERN)){
-			this.data = padronizarData(data);
+		}else{
+			// Nothing to do
 		}
-		else
-			throw new ReservaException(DATA_INVALIDA);
-		
+
+		data = data.trim();
+		if ( data.equals( "" ) ) { 
+			throw new ReservaException( DATA_BRANCA );
+		} else if ( data.matches( DATA_PATTERN ) ) {
+			this.data = padronizarData( data );
+		} else { 
+			throw new ReservaException( DATA_INVALIDA );
+		}
 	}
 
-	public boolean equals(Reserva obj) {
-		return (this.hora.equals(obj.getHora()) &&
-			this.data.equals(obj.getData()));
+	/**
+	 * Function to validate if an object passed is equal to an instancied object
+	 * @param obj
+	 * @return
+	 */
+	public boolean equals( Reserva obj ) {
+		return ( this.hora.equals( obj.getHora() ) && this.data.equals( obj
+				.getData() ) );
 	}
-	
+
 	@Override
+	/**
+	 * To string method of Reserva class
+	 */
 	public String toString() {
-		return "\nHora=" + this.hora 
-			+ "\nData=" + this.data;
+		return "\nHora=" + this.hora + "\nData=" + this.data;
 	}
-	
-	private static String padronizarData(String data){
-		String agora[] = dataAtual().split("[./-]");
-		String partes[] = data.split("[./-]");
+
+	/**
+	 * Method to standardize date format
+	 * @param data
+	 * @return String
+	 */
+	private static String padronizarData( String data ) {
+		String agora[] = dataAtual().split( "[./-]" );
+		String partes[] = data.split( "[./-]" );
 		String dataNoPadrao = "";
-		
-		for(int i = 0; i < 3; i++){
-			if(i == 0)
-				dataNoPadrao += agora[i].substring(0, 
-						agora[i].length() - partes[i].length()) + partes[i];
-			else
-				dataNoPadrao +=  "/" + agora[i].substring(0, 
-						agora[i].length() - partes[i].length()) + partes[i];
-				
+
+		for ( int i = 0; i < 3; i++ ) {
+			if ( i == 0 ) {
+				dataNoPadrao += agora[i].substring(0, agora[i].length()
+						- partes[i].length())
+						+ partes[i];
+			} else { 
+				dataNoPadrao += "/"
+						+ agora[i].substring(0,
+								agora[i].length() - partes[i].length())
+						+ partes[i];
+			}
 		}
-		
+
 		return dataNoPadrao;
 	}
-	
-	private static String dataAtual(){
+
+	/**
+	 * Method to get the current date from system
+	 * @return String
+	 */
+	private static String dataAtual() {
 		Date date = new Date(System.currentTimeMillis());
 		SimpleDateFormat formatador = new SimpleDateFormat("dd/MM/yyyy");
 		return formatador.format(date);
