7a996f90102c416dc0612bdb9bc77f173b26504c
Attany Araújo <nathalyaraujoo@gmail.com>
2015-03-29 17:03:30 -0300

    Applying stylesheet pattern in EquipamentoDao
    Change the if strucuture

--- Codigo/src/persistence/EquipamentoDAO.java
+++ Codigo/src/persistence/EquipamentoDAO.java
@@ -25,18 +25,20 @@ public class EquipamentoDAO {
     }
 
     public static EquipamentoDAO getInstance() {
-        if ( instance == null )
+        if ( instance == null ) {
             instance = new EquipamentoDAO();
+        }
         return instance;
     }
 
     //
 
     public void incluir(Equipamento equipamento) throws SQLException, PatrimonioException {
-        if ( equipamento == null )
+        if ( equipamento == null ) {
             throw new PatrimonioException( EQUIPAMENTO_NULO );
-        else if ( this.inDBCodigo( equipamento.getCodigo() ) )
+        } else if ( this.inDBCodigo( equipamento.getCodigo() ) ) {
             throw new PatrimonioException( CODIGO_JA_EXISTENTE );
+        }
         else if ( !this.inDB( equipamento ) ) {
             this.updateQuery( "INSERT INTO " + "equipamento ( codigo, descricao ) VALUES ( " + "\"" + equipamento.getCodigo() + "\", "
                     + "\"" + equipamento.getDescricao() + "\" );" );
@@ -44,21 +46,23 @@ public class EquipamentoDAO {
     }
 
     public void alterar( Equipamento old_equipamento, Equipamento new_equipamento ) throws SQLException, PatrimonioException {
-        if ( old_equipamento == null )
+        if ( old_equipamento == null ) {
             throw new PatrimonioException( EQUIPAMENTO_NULO );
-        if ( new_equipamento == null )
+        }
+        if ( new_equipamento == null ) {
             throw new PatrimonioException( EQUIPAMENTO_NULO );
+        }
 
         Connection con = FactoryConnection.getInstance().getConnection();
         PreparedStatement pst;
 
-        if ( !this.inDB( old_equipamento ) )
+        if ( !this.inDB( old_equipamento ) ) {
             throw new PatrimonioException( EQUIPAMENTO_NAO_EXISTENTE );
-        else if ( this.inOtherDB( old_equipamento ) )
+        } else if ( this.inOtherDB( old_equipamento ) ) {
             throw new PatrimonioException( EQUIPAMENTO_EM_USO );
-        else if ( !new_equipamento.getCodigo().equals( old_equipamento.getCodigo() ) && this.inDBCodigo( new_equipamento.getCodigo() ) )
+        } else if ( !new_equipamento.getCodigo().equals( old_equipamento.getCodigo() ) && this.inDBCodigo( new_equipamento.getCodigo() ) ) {
             throw new PatrimonioException( CODIGO_JA_EXISTENTE );
-        else if ( !this.inDB( new_equipamento ) ) {
+        } else if ( !this.inDB( new_equipamento ) ) {
             String msg = "UPDATE equipamento SET " + "codigo = \"" + new_equipamento.getCodigo() + "\", " + "descricao = \""
                     + new_equipamento.getDescricao() + "\"" + " WHERE " + "equipamento.codigo = \"" + old_equipamento.getCodigo()
                     + "\" and " + "equipamento.descricao = \"" + old_equipamento.getDescricao() + "\";";
@@ -70,21 +74,24 @@ public class EquipamentoDAO {
 
             pst.close();
 
-        } else
+        } else {
             throw new PatrimonioException( EQUIPAMENTO_JA_EXISTENTE );
+        }
         con.close();
     }
 
     public void excluir( Equipamento equipamento ) throws SQLException, PatrimonioException {
-        if ( equipamento == null )
+        if ( equipamento == null ) {
             throw new PatrimonioException( EQUIPAMENTO_NULO );
-        else if ( this.inOtherDB( equipamento ) )
+        } else if ( this.inOtherDB( equipamento ) ) {
             throw new PatrimonioException( EQUIPAMENTO_EM_USO );
+        }
         if ( this.inDB( equipamento ) ) {
             this.updateQuery( "DELETE FROM equipamento WHERE " + "equipamento.codigo = \"" + equipamento.getCodigo() + "\" and "
                     + "equipamento.descricao = \"" + equipamento.getDescricao() + "\";" );
-        } else
+        } else {
             throw new PatrimonioException( EQUIPAMENTO_NAO_EXISTENTE );
+        }
     }
 
     public Vector<Equipamento> buscarTodos() throws SQLException, PatrimonioException {
@@ -111,8 +118,9 @@ public class EquipamentoDAO {
         PreparedStatement pst = con.prepareStatement( query );
         ResultSet rs = pst.executeQuery();
 
-        while ( rs.next() )
+        while ( rs.next() ) {
             vet.add( this.fetchEquipamento( rs ) );
+        }
 
         pst.close();
         rs.close();
