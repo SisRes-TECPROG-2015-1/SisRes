9c3bb7a9d168f1620e5370500b582016db22f427
Macário Soares <macario.junior@gmail.com>
2015-03-29 00:15:27 -0300

    Updating model 'ReservaEquipamento', applying stylesheet and method's comments

--- Codigo/src/model/ReservaEquipamento.java
+++ Codigo/src/model/ReservaEquipamento.java
@@ -4,32 +4,60 @@ import exception.ReservaException;
 
 public class ReservaEquipamento extends Reserva {
 
-    private Equipamento equipamento;
+	private Equipamento equipamento;
 
-    // Mensagens
-    private final String EQUIPAMENTO_NULO = "O equipamneto esta nulo.";
+	// Message for exception
+	private final String EQUIPAMENTO_NULO = "O equipamneto esta nulo.";
 
-    public ReservaEquipamento(String data, String hora, Equipamento equipamento) throws ReservaException {
-        super(data, hora);
-        this.setEquipamento(equipamento);
-    }
+	/**
+	 * Constructor method for ReservaEquipamento class
+	 * @param data
+	 * @param hora
+	 * @param equipamento
+	 * @throws ReservaException
+	 */
+	public ReservaEquipamento( String data, String hora, Equipamento equipamento )
+			throws ReservaException {
+		super( data, hora );
+		this.setEquipamento( equipamento );
+	}
 
-    public Equipamento getEquipamento() {
-        return this.equipamento;
-    }
+	/**
+	 * Getter method for attribute 'equipamento'
+	 * @return
+	 */
+	public Equipamento getEquipamento() {
+		return this.equipamento;
+	}
 
-    public void setEquipamento(Equipamento equipamento) throws ReservaException {
-        if (equipamento == null)
-            throw new ReservaException(EQUIPAMENTO_NULO);
-        this.equipamento = equipamento;
-    }
+	/**
+	 * Setter method for attribute 'equipamento'
+	 * @param equipamento
+	 * @throws ReservaException
+	 */
+	public void setEquipamento( Equipamento equipamento ) throws ReservaException {
+		if ( equipamento == null )
+			throw new ReservaException( EQUIPAMENTO_NULO );
+		this.equipamento = equipamento;
+	}
 
-    public boolean equals(ReservaEquipamento obj) {
-        return (super.equals(obj) && this.getEquipamento().equals(obj.getEquipamento()));
-    }
+	/**
+	 * Function to validate if an object passed is equal to an instancied object
+	 * @param obj
+	 * @return
+	 */
+	public boolean equals( ReservaEquipamento obj ) {
+		return ( super.equals( obj ) && this.getEquipamento().equals( 
+				obj.getEquipamento() ) );
+	}
 
-    @Override public String toString() {
-        return "ReservaEquipamento [equipamento=" + this.getEquipamento() + ", toString()=" + super.toString() + "]";
-    }
+	@Override
+	/**
+	 * To string method for class ReservaEquipamento
+	 */
+	public String toString() {
+		return "ReservaEquipamento [equipamento=" + this.getEquipamento()
+				+ ", toString()=" + super.toString() + "]";
+	}
 
 }
