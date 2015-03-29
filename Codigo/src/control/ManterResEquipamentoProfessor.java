2c14d2e82ac80b6aa13feb2f9204fbbb63799614
Tainara Reis <tainara.s.r@hotmail.com>
2015-03-29 20:24:18 -0300

    Revert "Applying stylesheet patter in Control Classes and commentApplying stylesheet patten in Control Classes and commentariesries"
    This reverts commit 0807ad52c6535fe07747a9eb4ade89de6ab97b2b.

--- Codigo/src/control/ManterResEquipamentoProfessor.java
+++ Codigo/src/control/ManterResEquipamentoProfessor.java
@@ -12,8 +12,7 @@ import exception.PatrimonioException;
 import exception.ReservaException;
 
 public class ManterResEquipamentoProfessor {
-	
-    private Vector < Object > rev_equipamento_professor_vet = new Vector < Object >();
+    private Vector<Object> rev_equipamento_professor_vet = new Vector<Object>();
 
     // Singleton
     private static ManterResEquipamentoProfessor instance;
@@ -21,76 +20,47 @@ public class ManterResEquipamentoProfessor {
     private ManterResEquipamentoProfessor() {
     }
 
-    
-    /**
-	 * Creates an instance of a reserve if it isn't already instantiated.
-	 * @return - ManterResEquipamentoProfessor - Equipment reserve
-	 */
     public static ManterResEquipamentoProfessor getInstance() {
-        if ( instance == null )
+        if (instance == null)
             instance = new ManterResEquipamentoProfessor();
         return instance;
     }
 
+    //
 
-    /**
-     * Captures the teacher which had reserved a equipment in the hour searched.
-     * @return Vector - Teachers
-     */
-    public Vector < ReservaEquipamentoProfessor > getReservasHora( String hora ) throws SQLException, PatrimonioException,
+    public Vector<ReservaEquipamentoProfessor> getReservasHora(String hora) throws SQLException, PatrimonioException,
             ClienteException, ReservaException {
-        return ResEquipamentoProfessorDAO.getInstance().buscarPorHora( hora );
+        return ResEquipamentoProfessorDAO.getInstance().buscarPorHora(hora);
+
     }
 
-    /**
-     * Captures the teachers which had reserved a equipment in the month searched.
-     * @return Vector - Teachers
-     */
-    public Vector < ReservaEquipamentoProfessor > getReservasMes( int mes ) throws SQLException, PatrimonioException, ClienteException,
+    public Vector<ReservaEquipamentoProfessor> getReservasMes(int mes) throws SQLException, PatrimonioException, ClienteException,
             ReservaException {
-        return ResEquipamentoProfessorDAO.getInstance().buscarPorMes( mes );
+        return ResEquipamentoProfessorDAO.getInstance().buscarPorMes(mes);
     }
 
-    
-    /**
-     * Lists all the reserves.
-     * @return Vector - Equipment
-     */
-    public Vector < Object > getResEquipamentoProfessor_vet() throws SQLException, ClienteException, PatrimonioException,
+    public Vector<Object> getResEquipamentoProfessor_vet() throws SQLException, ClienteException, PatrimonioException,
             ReservaException {
         this.rev_equipamento_professor_vet = ResEquipamentoProfessorDAO.getInstance().buscarTodos();
         return this.rev_equipamento_professor_vet;
     }
 
-    
-    /**
-     * Reserves an equipment to a teacher to a date and hour.
-     */
-    public void inserir( Equipamento equipamento, Professor prof, String data, String hora ) throws SQLException, ReservaException {
-        ReservaEquipamentoProfessor reserva = new ReservaEquipamentoProfessor( data, hora, equipamento, prof );
-        ResEquipamentoProfessorDAO.getInstance().incluir( reserva );
-        this.rev_equipamento_professor_vet.add( reserva );
+    public void inserir(Equipamento equipamento, Professor prof, String data, String hora) throws SQLException, ReservaException {
+        ReservaEquipamentoProfessor reserva = new ReservaEquipamentoProfessor(data, hora, equipamento, prof);
+        ResEquipamentoProfessorDAO.getInstance().incluir(reserva);
+        this.rev_equipamento_professor_vet.add(reserva);
     }
 
-    
-    /**
-     * Changes the reserve to a new one.
-     */
-    public void alterar( String finalidade, ReservaEquipamentoProfessor reserva ) throws SQLException, ReservaException {
+    public void alterar(String finalidade, ReservaEquipamentoProfessor reserva) throws SQLException, ReservaException {
 
-        ReservaEquipamentoProfessor reserva_old = new ReservaEquipamentoProfessor( reserva.getData(), reserva.getHora(),
+        ReservaEquipamentoProfessor reserva_old = new ReservaEquipamentoProfessor(reserva.getData(), reserva.getHora(),
                 reserva.getEquipamento(), reserva.getProfessor());
-        ResEquipamentoProfessorDAO.getInstance().alterar( reserva_old, reserva );
+        ResEquipamentoProfessorDAO.getInstance().alterar(reserva_old, reserva);
 
     }
 
-    
-    /**
-     * Excludes a reserve.
-     */
-    public void excluir( ReservaEquipamentoProfessor reserva ) throws SQLException, ReservaException {
-        ResEquipamentoProfessorDAO.getInstance().excluir( reserva );
-        this.rev_equipamento_professor_vet.remove( reserva );
+    public void excluir(ReservaEquipamentoProfessor reserva) throws SQLException, ReservaException {
+        ResEquipamentoProfessorDAO.getInstance().excluir(reserva);
+        this.rev_equipamento_professor_vet.remove(reserva);
     }
-
 }
