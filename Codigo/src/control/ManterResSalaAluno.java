f96036ee00d6e54d8632098c5abba13a3d7ac226
Attany Araújo <nathalyaraujoo@gmail.com>
2015-04-05 13:23:30 -0300

    Commenting attributes, methods and parameters in ManterResSalaAlunoControl

--- Codigo/src/control/ManterResSalaAluno.java
+++ Codigo/src/control/ManterResSalaAluno.java
@@ -11,12 +11,23 @@ import exception.ClienteException;
 import exception.PatrimonioException;
 import exception.ReservaException;
 
+/**
+ * Equipment and rooms reservation system
+ * ManterResSalaAluno class with methods to accomplish CRUD reserves of classroom by a student
+ */
 public class ManterResSalaAluno {
 
+	/**
+	 * Creating a vector of reserves of classroom by a student
+	 */
 	private Vector<ReservaSalaAluno> rev_sala_aluno_vet = new Vector<ReservaSalaAluno>();
-	//Singleton
+
+	//This attribute is an instance of a ManterResSalaAluno type
 	private static ManterResSalaAluno instance;
 
+	/**
+	 * Empty constructor
+	 */
 	private ManterResSalaAluno() {
 	}
 
@@ -35,6 +46,7 @@ public class ManterResSalaAluno {
 	
 	/**
      * Captures the classroom reserves for students in the date searched.
+     * Parameter hora used to bring information about reserve's hour
      * @return Vector - Classroom reserves
      */	
 	public Vector < ReservaSalaAluno > getReservasHora( String hora ) throws SQLException, PatrimonioException, ClienteException, ReservaException {
@@ -44,6 +56,7 @@ public class ManterResSalaAluno {
 	
 	/**
      * Captures the classroom reserves for teachers in the date searched.
+     * Parameter data used to bring information about reserve's data
      * @return Vector - Classroom reserves
      */	
 	public Vector < ReservaSalaAluno > getReservasMes( String data ) throws SQLException, PatrimonioException, ClienteException, ReservaException {
@@ -62,6 +75,7 @@ public class ManterResSalaAluno {
 
 	/**
 	 * Captures the number of chairs available in a reserved classroom in a given date and hour
+	 * Parameter sala, data and hora used to bring information about available chairs
 	 * @return int - Available chairs in the classroom reserved 
 	 */
 	public int cadeirasDisponveis( Sala sala, String data, String hora ) throws SQLException, PatrimonioException, ClienteException, ReservaException {
@@ -71,6 +85,8 @@ public class ManterResSalaAluno {
 	
 	/**
      * Reserves a classroom to a student.
+     * Parameter sala, aluno, data, hora, finalidade and cadeiras_reservadas used to bring information for 
+     * create reserves of classroom by a student
      */
 	public void inserir( Sala sala, Aluno aluno,
 		String data, String hora, String finalidade, String cadeiras_reservadas )
@@ -84,6 +100,7 @@ public class ManterResSalaAluno {
 	
 	/**
      * Changes the classroom reserve to a new one.
+     * Parameter finalidade, cadeiras_reservadas used to update the reserve
      */
 	public void alterar( String finalidade, String cadeiras_reservadas, ReservaSalaAluno r )
 		throws SQLException, ReservaException, ClienteException, PatrimonioException {
