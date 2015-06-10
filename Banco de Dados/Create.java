dd0cf42abc7f162cc86cbf83c78852d839be64af
Laércio Silva <laerciossjr@gmail.com>
2015-06-10 11:11:52 -0300

    New DATABASE with translation

--- Banco de Dados/Create.java
+++ Banco de Dados/Create.java
@@ -7,84 +7,108 @@ import java.sql.SQLException;
 public class Create {
 
 	public static void main(String[] args) {
-		uptodate("CREATE TABLE aluno ( id_aluno INT NOT NULL AUTO_INCREMENT, " +
-				"nome VARCHAR(100) NOT NULL, " +
+		//Deletador de banco
+		reconfig("DROP DATABASE sisres_db;");
+		reconfig("CREATE DATABASE sisres_db;");
+		//Criador de tabelas
+		uptodate("CREATE TABLE student ( id_student INT NOT NULL AUTO_INCREMENT, " +
+				"name VARCHAR(100) NOT NULL, " +
 				"cpf VARCHAR(14) NOT NULL, " +
-				"telefone VARCHAR(15), " +
+				"fone VARCHAR(15), " +
 				"email VARCHAR(60), " +
-				"matricula VARCHAR(15) NOT NULL, " +
-				"PRIMARY KEY (id_aluno));");
+				"registration VARCHAR(15) NOT NULL, " +
+				"PRIMARY KEY (id_student));");
 		
-		uptodate("CREATE TABLE equipamento ( " +
-				"id_equipamento INT NOT NULL AUTO_INCREMENT, " +
-				"codigo VARCHAR(15) NOT NULL, " +
-				"descricao VARCHAR(120), " +
-				"PRIMARY KEY (id_equipamento));");
+		uptodate("CREATE TABLE equipment ( " +
+				"id_equipment INT NOT NULL AUTO_INCREMENT, " +
+				"code VARCHAR(15) NOT NULL, " +
+				"description VARCHAR(120), " +
+				"PRIMARY KEY (id_equipment));");
 	
 	
-		uptodate("CREATE TABLE professor (" +
-				"id_professor INT NOT NULL AUTO_INCREMENT, " +
-				"nome VARCHAR(100) NOT NULL, " +
+		uptodate("CREATE TABLE teacher (" +
+				"id_teacher INT NOT NULL AUTO_INCREMENT, " +
+				"name VARCHAR(100) NOT NULL, " +
 				"cpf VARCHAR(14) NOT NULL, " +
-				"telefone VARCHAR(15), " +
+				"fone VARCHAR(15), " +
 				"email VARCHAR(60), " +
-				"matricula VARCHAR(15) NOT NULL, " +
-				"PRIMARY KEY (id_professor));");
+				"registration VARCHAR(15) NOT NULL, " +
+				"PRIMARY KEY (id_teacher));");
 	
 	
-		uptodate("CREATE TABLE reserva_equipamento ( " +
-				"id_reserva_equipamento INT NOT NULL AUTO_INCREMENT, " +
-				"id_professor INT NOT NULL, " +
-				"id_equipamento INT NOT NULL, " +
-				" hora VARCHAR(5) NOT NULL, " +
-				"data VARCHAR(10) NOT NULL, " +
-				"PRIMARY KEY (id_reserva_equipamento));");
+		uptodate("CREATE TABLE reservation_equipment ( " +
+				"id_reservation_equipment INT NOT NULL AUTO_INCREMENT, " +
+				"id_teacher INT NOT NULL, " +
+				"id_equipment INT NOT NULL, " +
+				" hour VARCHAR(5) NOT NULL, " +
+				"date VARCHAR(10) NOT NULL, " +
+				"PRIMARY KEY (id_reservation_equipment));");
 	
 	
-		uptodate("CREATE TABLE sala ( " +
-				"id_sala INT NOT NULL AUTO_INCREMENT, " +
-				"codigo VARCHAR(10) NOT NULL, " +
-				"descricao VARCHAR(120), " +
-				"capacidade INT, PRIMARY KEY (id_sala) );");
+		uptodate("CREATE TABLE room ( " +
+				"id_room INT NOT NULL AUTO_INCREMENT, " +
+				"code VARCHAR(10) NOT NULL, " +
+				"description VARCHAR(120), " +
+				"capacity INT, PRIMARY KEY (id_room) );");
 	
 	
-		uptodate("CREATE TABLE reserva_sala_aluno ( " +
-				"id_reserva_sala_aluno INT NOT NULL AUTO_INCREMENT, " +
-				"id_aluno INT NOT NULL, " +
-				"id_sala INT NOT NULL, " +
-				"finalidade VARCHAR(150) NOT NULL, " +
-				"hora VARCHAR(5) NOT NULL, " +
-				"data VARCHAR(10) NOT NULL, " +
-				"cadeiras_reservadas INT NOT NULL, " +
-				"PRIMARY KEY (id_reserva_sala_aluno));");
+		uptodate("CREATE TABLE reservation_room_student ( " +
+				"id_reservation_room_student INT NOT NULL AUTO_INCREMENT, " +
+				"id_student INT NOT NULL, " +
+				"id_room INT NOT NULL, " +
+				"finality VARCHAR(150) NOT NULL, " +
+				"hour VARCHAR(5) NOT NULL, " +
+				"date VARCHAR(10) NOT NULL, " +
+				"reserved_chairs INT NOT NULL, " +
+				"PRIMARY KEY (id_reservation_room_student));");
 	
 	
-		uptodate("CREATE TABLE reserva_sala_professor (" +
-				"id_reserva_sala_professor INT NOT NULL AUTO_INCREMENT, " +
-				"id_professor INT NOT NULL, " +
-				"id_sala INT NOT NULL, " +
-				"finalidade VARCHAR(150) NOT NULL, " +
-				"hora VARCHAR(5) NOT NULL, " +
-				"data VARCHAR(10) NOT NULL, " +
-				"PRIMARY KEY (id_reserva_sala_professor));");
+		uptodate("CREATE TABLE reservation_room_teacher (" +
+				"id_reservation_room_teacher INT NOT NULL AUTO_INCREMENT, " +
+				"id_teacher INT NOT NULL, " +
+				"id_room INT NOT NULL, " +
+				"finality VARCHAR(150) NOT NULL, " +
+				"hour VARCHAR(5) NOT NULL, " +
+				"date VARCHAR(10) NOT NULL, " +
+				"PRIMARY KEY (id_reservation_room_teacher));");
 	
 	
-		uptodate("ALTER TABLE reserva_equipamento ADD CONSTRAINT FK_reserva_equipamento_0 FOREIGN KEY (id_professor) REFERENCES professor (id_professor);");
-		uptodate("ALTER TABLE reserva_equipamento ADD CONSTRAINT FK_reserva_equipamento_1 FOREIGN KEY (id_equipamento) REFERENCES equipamento (id_equipamento);");
+		uptodate("ALTER TABLE reservation_equipment ADD CONSTRAINT FK_reservation_equipment_0 FOREIGN KEY (id_teacher) REFERENCES teacher (id_teacher);");
+		uptodate("ALTER TABLE reservation_equipment ADD CONSTRAINT FK_reservation_equipment_1 FOREIGN KEY (id_equipment) REFERENCES equipment (id_equipment);");
 	
 	
-		uptodate("ALTER TABLE reserva_sala_aluno ADD CONSTRAINT FK_reserva_sala_aluno_0 FOREIGN KEY (id_aluno) REFERENCES aluno (id_aluno);");
-		uptodate("ALTER TABLE reserva_sala_aluno ADD CONSTRAINT FK_reserva_sala_aluno_1 FOREIGN KEY (id_sala) REFERENCES sala (id_sala);");
+		uptodate("ALTER TABLE reservation_room_student ADD CONSTRAINT FK_reservation_room_student_0 FOREIGN KEY (id_student) REFERENCES student (id_student);");
+		uptodate("ALTER TABLE reservation_room_student ADD CONSTRAINT FK_reservation_room_student_1 FOREIGN KEY (id_room) REFERENCES room (id_room);");
 	
 	
-		uptodate("ALTER TABLE reserva_sala_professor ADD CONSTRAINT FK_reserva_sala_professor_0 FOREIGN KEY (id_professor) REFERENCES professor (id_professor);");
-		uptodate("ALTER TABLE reserva_sala_professor ADD CONSTRAINT FK_reserva_sala_professor_1 FOREIGN KEY (id_sala) REFERENCES sala (id_sala);");
+		uptodate("ALTER TABLE reservation_room_teacher ADD CONSTRAINT FK_reservation_room_teacher_0 FOREIGN KEY (id_teacher) REFERENCES teacher (id_teacher);");
+		uptodate("ALTER TABLE reservation_room_teacher ADD CONSTRAINT FK_reservation_room_teacher_1 FOREIGN KEY (id_room) REFERENCES room (id_room);");
 		
 
 	}
 	public static void uptodate(String msg)
 	{
 		String local = "jdbc:mysql://localhost/sisres_db";
+		String user = "root";
+		String password = "root";
+		
+		Connection con = null;
+		PreparedStatement pst = null;
+		try {
+			con = null;
+			con = DriverManager.getConnection(local, user, password);
+			pst = con.prepareStatement(msg);
+			pst.executeUpdate();
+			pst.close();
+			con.close();
+			System.out.println("Ok!");
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}	
+	}
+	public static void reconfig(String msg)
+	{
+		String local = "jdbc:mysql://localhost";
 		String user = "root";
 		String password = "root";
 		
